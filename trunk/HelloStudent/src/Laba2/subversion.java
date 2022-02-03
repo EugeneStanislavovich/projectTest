c874dbef6e4f31c4756dff50ff154194c29c7824
user125 <user125@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-02-03 23:07:29 +0000

    Импорт проекта HelloStudent из Eclipse

---
+++ trunk/HelloStudent/src/Laba2/subversion.java
@@ -0,0 +1,44 @@
+package Laba2; //пакет Java классов
+
+import java.awt.event.ActionListener; //библиотека для события слушателя
+import javax.swing.*; // Библиотека для GUI (построена на основе awt)
+
+public class subversion {
+
+	public subversion() {
+	JFrame main_GUI = new JFrame("subversion");	// создание графического окна
+	main_GUI.setTitle ("Using subversion for developers");
+	main_GUI.setBounds(500,400,400,300);
+	main_GUI.setResizable(false); // фиксированный размер окна
+	
+	
+	JPanel main_panel = new JPanel(); // Панель - используется для организации компонентов в окне
+	main_panel.setLayout(null);
+	main_GUI.add(main_panel);
+	
+	JLabel laba_info = new JLabel("Лабораторная работа №2"); // Отображение текста или изображения
+	laba_info.setBounds(120,0,150,30);
+	main_panel.add(laba_info);
+	
+	JButton button_exit = new JButton("Выход"); // добавляем кнопку
+	button_exit.setBounds(270,200,100,40);
+	ActionListener actionListener = new ListenerButton(); //создаем слушатель
+	button_exit.addActionListener(actionListener); // добавляем слушатель к кнопке
+	main_panel.add(button_exit);
+	
+	
+	
+	main_GUI.setVisible(true);
+	
+	main_GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // закрытие окна JFrame и процесса Java
+	}
+	
+	
+	
+	
+	public static void main(String[] args) { // интересные заметки: http://www.mstu.edu.ru/study/materials/java/
+		subversion student= new subversion();
+		
+	}
+
+}
