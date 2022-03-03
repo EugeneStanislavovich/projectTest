18c8880df0d6f0b297e5626ff154c5754dc52eb0
user225 <user225@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-03-03 13:54:22 +0000

    Подправил предыдущую ревизию

--- HelloStudent/src/Laba2/Information.java
+++ HelloStudent/src/Laba2/Information.java
@@ -6,10 +6,11 @@ import javax.swing.JOptionPane;
 public class Information implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String message = "ФИО и номера зачетных книжек\n";
-        message += "Налигацкий Евгений 20150096\n";
-        message += "Аминев Алексей 20130087\n";
-        message += "Вяхирев Алексей 20130485\n";
-        message += "Масленникова Ванесса 20130840\n";
+        message += "-------------------------------\n";
+        message += "Налигацкий Евгений | 20150096\n";
+        message += "Аминев Алексей | 20130087\n";
+        message += "Вяхирев Алексей | 20130485\n";
+        message += "Масленникова Ванесса | 20130840\n";
         
         JOptionPane.showMessageDialog(null, message , "Сообщение" , JOptionPane.PLAIN_MESSAGE);
     }
