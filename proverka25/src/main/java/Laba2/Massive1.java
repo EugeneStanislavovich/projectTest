2d2326c99052a080a5b516da189a1c560387c755
user325 <user325@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-03-28 04:26:55 +0000

    Добавлен класс Sorting для пузырьковой сортировки массива

--- proverka25/src/main/java/Laba2/Massive1.java
+++ proverka25/src/main/java/Laba2/Massive1.java
@@ -1,6 +1,7 @@
 package Laba2;
 
 public class Massive1 {
+	static Integer schet = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,9 +15,40 @@ public class Massive1 {
 		while (i <= 6) {
 			Massive[i] = (int) (Math.random() * ++max) + min;
 			i++;
+			schet++;
 			
 		}
+		
+		
+		class Sorting extends Massive1 {
+
+
+			public void sort() {
+				for (int out = schet - 1; out >= 1; out--) {
+					for (int in = 0; in < out; in++) {
+						if (Massive[in] > Massive[in+1]) {
+							Integer time = Massive[in];
+							Massive[in] = Massive[in+1];
+							Massive[in+1] = time;
+						}
+					}
+				}
+			
+			}
+
+
+		}
+		Sorting sorting = new Sorting();
+		sorting.sort();
+		
+		while(schet >=1 ) {     
+			schet--;
+			System.out.println(Massive[schet]);
+		}
+
 
 	}
+
+
 
 }
