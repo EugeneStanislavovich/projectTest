074431b6d7b500251e193dbf3f82aecaa87817ac
user325 <user325@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-03-29 05:34:33 +0000

    Исправление структуры классов для лабораторной №5, для разработчиков 325 и 425.

--- branches/BranchDEV_Aminev/proverka25/src/main/java/Laba2/Massive1.java
+++ branches/BranchDEV_Aminev/proverka25/src/main/java/Laba2/Massive1.java
@@ -1,54 +1,23 @@
 package Laba2;
 
 public class Massive1 {
-	static Integer schet = 0;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public Integer[] Massive2(int j){
+	
+	Integer[] Massive = new Integer[j];
+	Integer schet = 0;
+	//Integer i = 0;
+	//max -= min;
+	for (Integer i = 0; i <= 6; i++) {
 		Integer min = 50; // Минимальное число для диапазона
 		Integer max = 95; // Максимальное число для диапазона
-		
-
-		Integer[] Massive = new Integer[7];
-		Integer i = 0;
 		max -= min;
-		while (i <= 6) {
-			Massive[i] = (int) (Math.random() * ++max) + min;
-			i++;
-			schet++;
-			
-		}
+		Massive[i] = (int) (Math.random() * ++max) + min;
+		//i++;
+		schet++;
 		
-		
-		class Sorting extends Massive1 {
-
-
-			public void sort() {
-				for (int out = schet - 1; out >= 1; out--) {
-					for (int in = 0; in < out; in++) {
-						if (Massive[in] > Massive[in+1]) {
-							Integer time = Massive[in];
-							Massive[in] = Massive[in+1];
-							Massive[in+1] = time;
-						}
-					}
-				}
-			
-			}
-
-
-		}
-		Sorting sorting = new Sorting();
-		sorting.sort();
-		
-		while(schet >=1 ) {     
-			schet--;
-			System.out.println(Massive[schet]);
-		}
-
-
 	}
-
-
+	return Massive;
+}
 
 }
