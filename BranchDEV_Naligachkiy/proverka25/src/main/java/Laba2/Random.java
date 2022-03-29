d56aef24807668aa5835495a93bc35a58d38edb6
user125 <user125@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-03-29 08:46:47 +0000

    Добавил метку для ответвления Налигацкого

---
+++ BranchDEV_Naligachkiy/proverka25/src/main/java/Laba2/Random.java
@@ -0,0 +1,13 @@
+package Laba2;
+
+public class Random {
+	public static Integer Rand() {
+		Integer min = 50; // Минимальное число для диапазона
+		Integer max = 95; // Максимальное число для диапазона
+		max -= min;
+		Integer Random = (int) (Math.random() * ++max) + min;
+		return Random;
+	}
+
+}
+
