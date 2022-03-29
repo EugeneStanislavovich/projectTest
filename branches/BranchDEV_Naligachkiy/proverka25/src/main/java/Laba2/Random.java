1b5a80deb6344a6436b9187353f155a5ee2ee9fa
user225 <user225@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-03-29 06:09:53 +0000

    Корректировка классы Random, и завершение Первого теста. Функция теста - проверка входимости генерируемого числа в заданный диапозон.

--- branches/BranchDEV_Naligachkiy/proverka25/src/main/java/Laba2/Random.java
+++ branches/BranchDEV_Naligachkiy/proverka25/src/main/java/Laba2/Random.java
@@ -1,16 +1,13 @@
 package Laba2;
 
 public class Random {
-	final int min = 50; // Минимальное число для диапазона
-	final int max = 95; // Максимальное число для диапазона
-	final int rnd = rnd(min, max);
-	
-	public static int rnd(int min, int max)
-	{
+	public static Integer Rand() {
+		Integer min = 50; // Минимальное число для диапазона
+		Integer max = 95; // Максимальное число для диапазона
 		max -= min;
-		return (int) (Math.random() * ++max) + min;
+		Integer Random = (int) (Math.random() * ++max) + min;
+		return Random;
 	}
-	
 
 }
 
